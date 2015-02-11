7b77e88e0fa4df19e8eb8fee4bdd2b9691b97e6c
olivesystem <olivesystem@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-11 05:50:42 +0000

    1.0.63+1
    git-svn-id: https://plugins.svn.wordpress.org/os-attendance-management/trunk@1086937 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include_files/admin-head.php
@@ -0,0 +1,12 @@
+<?php
+if(class_exists('AttendanceAdmin')){
+?>
+
+		<div id="attendance-header" class="clearfix">
+			<div class="plugin-name">出勤・勤怠管理プラグイン 1.0.63</div>
+			<div class="header-link"><a href="http://olivesystem.jp/lp/wordpress" target="_blank">サイト制作</a> | <a href="http://olivesystem.jp/lp/wordpress%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3%E9%96%8B%E7%99%BA" target="_blank">プラグイン開発</a> | <a href="http://olivesystem.jp/lp/license#attendance" target="_blank">ライセンス取得</a></div>
+		</div>
+
+<?php
+}
+?>
